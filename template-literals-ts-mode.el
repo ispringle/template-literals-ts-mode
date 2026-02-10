30d2f1a82029fb698fc2850b110395affaec4b78
Ian S. Pringle <ian@dapringles.com>
2026-02-10 09:23:55 -0600

    chore: add license to src file and add email to author line

--- template-literals-ts-mode.el
+++ template-literals-ts-mode.el
@@ -1,13 +1,25 @@
 ;;; template-literals-ts-mode.el --- Tree-sitter support for HTML/CSS in JS/TS template literals -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2025
-
-;; Author: Ian S. Pringle
+;; Author: Ian S. Pringle <ian@dapringles.org>
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "30.1"))
 ;; Keywords: languages, javascript, typescript, tree-sitter
 ;; URL: https://github.com/ispringle/template-literals-ts-mode
 ;; License: 0BSD
+;; SPDX-License-Identifier: 0BSD
+
+;; Copyright (C) 2026 by Ian S. Pringle ian@dapringles.com
+;; 
+;; Permission to use, copy, modify, and/or distribute this software for any
+;; purpose with or without fee is hereby granted.
+;; 
+;; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+;; WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+;; MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+;; ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+;; WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+;; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 ;;; Commentary:
 
